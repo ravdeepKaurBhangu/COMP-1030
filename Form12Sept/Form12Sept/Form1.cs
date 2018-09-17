2a4b3567a01beae6cd747c965e9b78889d3e686b
ravdeepKaurBhangu <40557322+ravdeepKaurBhangu@users.noreply.github.com>
2018-09-17 10:35:26 +0530

    Add files via upload

---
+++ Form12Sept/Form12Sept/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form12Sept
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fname;
+            string lname;
+            fname = textBox1.Text;
+            lname = textBox2.Text;
+            MessageBox.Show("Welcome "+fname+" "+lname);
+        }
+    }
+}
